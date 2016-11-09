a2e2ab4db2438a85f12684dd2fe0fbcdbc07167f
Jeffrey Lin <jeffrey@icurse.nl>
2016-11-08 20:35:53 -0500

    Fix /boot mountpoint detection.

--- mkkernel.sh
+++ mkkernel.sh
@@ -1,19 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ## Configure and compile kernel
 set -euo pipefail
 
 if [[ $(id -u) -ne 0 ]]; then
-    if hash sudo 2>/dev/null; then
-        printf "Restarting with root permissions using sudo.\n"
-        sudo bash "$0" "$@"
-        exit $?
-    else
-        printf "Script must be run as root!\n"
-        exit 1
-    fi
+    printf "Script must be run as root!\n"
+    exit 1
 fi
 
-MAKEOPTS="-j4"
+MAKEOPTS=("-j4")
 CWD=$(pwd)
 
 BOOT=
@@ -25,14 +19,14 @@ back() {
 trap back EXIT
 
 cd /usr/src/linux || exit
-make $MAKEOPTS nconfig
-make $MAKEOPTS
-make $MAKEOPTS modules_prepare
+make "${MAKEOPTS[@]}" nconfig
+make "${MAKEOPTS[@]}"
+make "${MAKEOPTS[@]}" modules_prepare
 
-if [ -z ${BOOT+x} ]; then mount /boot; fi
-make $MAKEOPTS install
-make $MAKEOPTS modules_install
+if [ $BOOT = 1 ]; then mount /boot; fi
+make "${MAKEOPTS[@]}" install
+make "${MAKEOPTS[@]}" modules_install
 genkernel --lvm initramfs
 grub-mkconfig -o /boot/grub/grub.cfg
-if [ -z ${BOOT+x} ]; then umount /boot; fi
+if [ $BOOT = 1 ]; then umount /boot; fi
 
