e44c8afd19088de5a84999e9fad993b4e2215b02
Jeffrey Lin <jeffrey@icurse.nl>
2017-09-17 17:28:17 -0400

    checkLowBattery.sh: implement emergency poweroff

--- checkLowBattery.sh
+++ checkLowBattery.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 POWERSUPPLY="/sys/class/power_supply/BAT1/status"
 TOO_LOW=30
+LOWEST=5
 NOT_CHARGING="Discharging"
 ICON="/usr/share/icons/HighContrast/48x48/status/battery-caution.png"
 
@@ -13,7 +14,14 @@ STATUS=$(cat $POWERSUPPLY)
 
 if [ "$BATTERY_LEVEL" -le "$TOO_LOW" ] && [ "$STATUS" = "$NOT_CHARGING" ]; then
     /usr/bin/notify-send -u critical -i "$ICON" "Battery low" "Battery level is ${BATTERY_LEVEL}%!"
+    exit 0
 fi
+
+if [ "$BATTERY_LEVEL" -le "$LOWEST" ] && [ "$STATUS" = "$NOT_CHARGING" ]; then
+    /usr/sbin/shutdown -h now
+    exit 0
+fi
+
 
 exit 0
 
