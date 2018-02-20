632ddb8ea272efe78a2af26f99f4201b27447ba4
Jeffrey Lin <jeffrey@icurse.nl>
2018-02-20 11:59:19 -0500

    update_gentoo.sh: nice script instead of children

--- sysutil/update_gentoo.sh
+++ sysutil/update_gentoo.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+renice +10 $$
+
 OPTIONS=('--alert' '--ask' '--backtrack=10000' '--binpkg-changed-deps' '--binpkg-respect-use' '--keep-going' '--quiet' '--verbose-conflicts')
 JOBS=1
 
@@ -12,9 +14,9 @@ fi
 
 eix-sync -q
 
-nice emerge "${OPTIONS[@]}" --jobs=${JOBS:-1} "$@" --complete-graph --deep --newuse --update --usepkg --with-bdeps=y @world
-nice emerge "${OPTIONS[@]}" --jobs=${JOBS:-1} --depclean
-nice emerge "${OPTIONS[@]}" --jobs=${JOBS:-1} --oneshot @preserved-rebuild
+emerge "${OPTIONS[@]}" --jobs=${JOBS:-1} "$@" --complete-graph --deep --newuse --update --usepkg --with-bdeps=y @world
+emerge "${OPTIONS[@]}" --jobs=${JOBS:-1} --depclean
+emerge "${OPTIONS[@]}" --jobs=${JOBS:-1} --oneshot @preserved-rebuild
 
 revdep-rebuild --quiet
 emaint all --fix
@@ -23,3 +25,4 @@ eclean-dist --deep --time-limit=1m --quiet
 eclean-pkg --deep --time-limit=1m --quiet
 
 prelink -amR
+
